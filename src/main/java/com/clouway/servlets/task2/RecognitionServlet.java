d8495558652732926aef16a20057dac17fe227cc
Zornica <zornicaewgeniewa@gmail.com>
2015-10-16 13:45:52 +0300

    rename ThreePagesServlet to RecognitionServlet

--- src/main/java/com/clouway/servlets/task2/RecognitionServlet.java
+++ src/main/java/com/clouway/servlets/task2/RecognitionServlet.java
@@ -11,7 +11,7 @@ import java.io.PrintWriter;
 /**
  * Created by clouway on 15-10-13.
  */
-public class ThreePagesServlet extends HttpServlet{
+public class RecognitionServlet extends HttpServlet{
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
