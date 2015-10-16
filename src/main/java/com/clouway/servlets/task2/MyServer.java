49292c9896077523a16b255af3d84f9d07498381
Zornica <zornicaewgeniewa@gmail.com>
2015-10-16 11:05:10 +0300

    task2  finished

---
+++ src/main/java/com/clouway/servlets/task2/MyServer.java
@@ -0,0 +1,21 @@
+package com.clouway.servlets.task2;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+
+/**
+ * Created by clouway on 15-10-16.
+ */
+public class MyServer {
+    public static void main(String[] args) throws Exception {
+
+        Server server = new Server(8080);
+        WebAppContext webAppcontext = new WebAppContext();
+        webAppcontext.setContextPath("/");
+        webAppcontext.setWar("src/main/webapp");
+        server.setHandler(webAppcontext);
+        server.start();
+        server.join();
+
+    }
+}
