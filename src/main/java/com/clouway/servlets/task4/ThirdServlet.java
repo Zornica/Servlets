49c08c695ab15e3e6a68371ab1aed705c554ddc6
Zornica <zornicaewgeniewa@gmail.com>
2015-10-21 12:02:53 +0300

    task4

--- src/main/java/com/clouway/servlets/task4/ThirdServlet.java
+++ src/main/java/com/clouway/servlets/task4/ThirdServlet.java
@@ -17,7 +17,7 @@ public class ThirdServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        resp.setContentType("text/html;charset=UTF-8");
+        resp.setContentType("text/html");
         PrintWriter out = resp.getWriter();
         ServletContext sc = getServletContext();
         sc.setAttribute("servletName", "Third servlet");
